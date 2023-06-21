52f6489566c9f179db139ac55055cb714199c463
AnonGhostID <adamakbar26@gmail.com>
2023-06-21 17:01:16 +0700

    d

--- resources/views/karyawan.blade.php
+++ resources/views/karyawan.blade.php
@@ -1,7 +1,7 @@
 @extends('layout.app')
 @section('title', 'Dashboard')
 @section('content')
-    <h1 class="h3 mb-2 text-gray-800">Artikel Diaryy</h1>
+    <h1 class="h3 mb-2 text-gray-800">Artikel Diaryyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy</h1>
     @if (session('pesan'))
         <div class="alert alert-success alert-dismissible fade show" role="alert">
             {{ session('pesan') }}
